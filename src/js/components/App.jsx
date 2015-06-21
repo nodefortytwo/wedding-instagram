cd83bfb5414b92dd287dea8430e3f3bb88255919
Rick Burgess <rick.burgess@bbh.co.uk>
2015-06-21 18:55:33 +0100

    awesome

---
+++ src/js/components/App.jsx
@@ -0,0 +1,109 @@
+var React = require('react');
+var DataStore = require('../stores/DataStore');
+var ActionCreator = require('../actions/DataActionCreators');
+var $ = require('jquery');
+
+var Row = require('./Row.jsx');
+var InstagramImage = require('./InstagramImage.jsx');
+var Col = require('./Col.jsx');
+
+var client_id = '570aebea4a644601bc07d4d8374e1706';
+var tag = 'DunneBurgess';
+var access_token = '';
+var max_id = 0;
+
+var App = React.createClass({
+
+  getInitialState: function() {
+//    var data = DataStore.getAll();
+//    console.log('test');
+    return {
+      images: []
+    }
+  },
+
+  _onChange: function() {
+    this.setState(DataStore.getAll());
+  },
+
+  componentDidMount: function() {
+    DataStore.addChangeListener(this._onChange);
+
+
+
+    if(window.location.hash.length > 1){
+      access_token = window.location.hash.substring(14);
+    }else{
+      window.location = 'https://instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='
+                            +window.location.origin+'&response_type=token'
+    }
+
+   this.loadData();
+
+  },
+
+  componentWillUnmount: function() {
+    DataStore.removeChangeListener(this._onChange);
+  },
+
+  handleClearListClick: function(e) {
+    ActionCreator.clearList();
+  },
+
+  loadData: function(){
+    var url = 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?count=24&access_token='+access_token + '&max_tag_id=' + max_id ;
+    var scope = this;
+    $.ajax({
+      url: url,
+      dataType: "jsonp",
+
+    }).done(function(data){
+      scope.setState({images: data.data});
+      max_id = data.data[0].id;
+      window.setTimeout(scope.loadData, 2000);
+    });
+  },
+
+  render: function() {
+
+    var rows = [];
+    rows.push(<Col md={2} key="placeholder" className="placeholder"><div><img src="/images/logo.png"/><h2>#DunneBurgess</h2></div></Col>);
+
+    for (var i=0; i < this.state.images.length; i++) {
+        rows.push(<InstagramImage key={this.state.images[i].id} instagramId={this.state.images[i].link} />);
+    }
+
+    return (
+      <div className="container-fluid">
+        <Row>
+          {rows}
+        </Row>
+        <div className="footer">
+          <strong>Share your pictures at #DunneBurgess</strong>
+        </div>
+      </div>
+    );
+  },
+
+  shuffle: function(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+});
+
+module.exports = App;
---
+++ src/js/components/InstagramImage.jsx
@@ -0,0 +1,47 @@
+var React = require('react');
+var joinClasses = require('./utils/joinClasses');
+
+var Col = require('./Col.jsx');
+
+var InstagramImage = React.createClass({
+  getInitialState: function() {
+    return {
+      loaded: false
+    };
+  },
+  
+  componentDidMount: function() {
+
+    // You may want to rename the component if the <Image> definition
+    // overrides window.Image
+    var scope = this;
+    var img = new window.Image();
+    img.onload = function(){
+    	scope.state.loaded = true;
+    };
+    img.src = this.props.instagramId + 'media';
+  },
+
+  render: function () {
+
+  	var url = this.props.instagramId + 'media';
+
+  	if (this.state.loaded) {
+	    return (
+	    	<Col md={2} className="instagramImage col fade-in">
+	    		<img src="/images/mask.png" className="mask"/>
+	    		<img src={url} width="100%"/>
+	    	</Col>
+	    );
+	}else{
+		return (
+	    	<Col md={2} className="instagramImage col hide">
+	    		<img src="/images/mask.png" className="mask"/>
+	    		
+	    	</Col>
+	    );
+	}
+  }
+});
+
+module.exports = InstagramImage;
